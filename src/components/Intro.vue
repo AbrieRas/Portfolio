954b4cf7447b464f8d1130834ecae85372f7bb9b
Abrie PC <www.headoff.com@gmail.com>
2025-05-26 11:57:21 +0200

    Add name to intro

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -7,7 +7,7 @@
         <div id="title"><h1>Full Stack Developer | Cybersecurity Analyst</h1></div>
         <div><img src="../assets/self.jpg"/></div>
         <div id="about-title"><h3>About me</h3></div>
-        <div><p>I am a dedicated software engineer, specializing in full-stack web development. 
+        <div><p>Good day, I'm Abrie! I am a dedicated software engineer, specializing in full-stack web development. 
             With a track record of delivering high-quality service and a passion for technology, 
             I am driven to tackle complex challenges and continuously evolving and growing in my 
             role as a software engineer.</p></div>
